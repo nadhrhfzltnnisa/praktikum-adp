bd104e885af370b760c97abe027aaca29a02a1a8
Naadhirah Fazilatun Nisa <155242684+nadhrhfzltnnisa@users.noreply.github.com>
2024-05-14 23:15:36 +0700

    Update TUGAS 7 MODUL 7 PRAK-ADP.py

--- TUGAS 7 MODUL 7 PRAK-ADP.py
+++ TUGAS 7 MODUL 7 PRAK-ADP.py
@@ -13,6 +13,12 @@ def input_data(n):
 n = int(input(" Input jumlah data = "))
 data = input_data(n)
 
+print()
+print(" +------------------------------+")
+print(" | {:<10} | {:<15} |".format(" Statistik", " Hasil"))
+print(" +------------------------------+")
+
+
 def mean(s) :
     data1 = 0
     for i in range (s) :
@@ -20,23 +26,23 @@ def mean(s) :
     rata2 = data1/s
     return rata2 
 s = len(data)
-print(" Mean data ini adalah = ", mean(s))
+hasil_mean = mean(s)
+print(" | {:<10} | {:<15} |".format("Mean", hasil_mean))
     
 def modus(s) :
-    data2 = { }
-    for i in data:
-        if i in data2 :
-            data2[i] += 1
-        else:
-            data2[i] = 1
-    maxnilai = max(data2.values())
+    data2 = [ 0 ] * (max(data) + 1)
+    for i in data :
+        data2[ i ] += 1
+    maxnilai = max(data2)
     modus = [ ]
-    for i, j in data2.items() :
-        if j == maxnilai:
-            modus.append(i)
+    for i in data :
+        if data2[ i ] == maxnilai :
+            if i not in modus :
+                modus.append(i)
     return modus
 s = len(data)
-print(" Modus data ini adalah = ", modus(s))
+hasil_modus = modus(s)
+print(" | {:<10} | {:<15} |".format("Modus", ', '.join(map(str, hasil_modus))))
 	
 def variance(s) :
 	rata2 = mean(s)
@@ -46,4 +52,7 @@ def variance(s) :
 		sigmaselisihkuadrat += selisih**2
 	varians = sigmaselisihkuadrat / (s - 1)
 	return varians
-print(" Variance data ini adalah = ", variance(s))
+hasil_variance = variance(s)
+print(" | {:<10} | {:<15} |".format("Variance", hasil_variance))
+
+print(" +______________________________+")
