16ec9f1aab3454afaf8010cfc98de536af297d93
Naadhirah Fazilatun Nisa <155242684+nadhrhfzltnnisa@users.noreply.github.com>
2024-05-28 22:06:34 +0700

    Create TUGAS 9 MODUL 9 PRAK-ADP.py

---
+++ TUGAS 9 MODUL 9 PRAK-ADP.py
@@ -0,0 +1,98 @@
+print(" Nama  : Naadhirah Fazilatun Nisa")
+print(" NIM   : 2310433021")
+print(" Shift : 3")
+print()
+print(" Program Logo MIT")
+print()
+
+from termcolor import colored, cprint
+
+for a in range (4) :
+	cprint(" "*5, "red", "on_light_red", end = " ")
+	print(" ", end = " ")
+cprint(" "*12, "red", "on_light_red", end = " ")
+kata1 = colored(" MASSACHUSETTS", "red")
+print(kata1)
+
+for b in range (4) :
+	cprint(" "*5, "red", "on_light_red", end = " ")
+	print(" ", end = " ")
+cprint(" "*12, "red", "on_light_red")
+
+for c in range (3) :
+	cprint(" "*5, "red", "on_light_red", end = " ")
+	print(" ", end = " ")
+print(" "*20, end = " ")
+kata2 = colored(" INSTITUTE OF", "red")
+print(kata2)
+
+for d in range (3) :
+	cprint(" "*5, "red", "on_light_red", end = " ")
+	print(" ", end = " ")
+print()
+
+for e in range (3) :
+	cprint(" "*5, "red", "on_light_red", end = " ")
+	print(" ", end = " ")
+print(" "*20, end = " ")
+kata2 = colored(" TECHNOLOGY", "red")
+print(kata2)
+
+for f in range(3) :
+    cprint(" "*5, "red", "on_light_red", end = " ")
+    print(" ", end = " ")
+cprint(" "*5, "grey", "on_dark_grey", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "red", "on_light_red")
+
+for g in range (3) :
+    cprint(" "*5, "red", "on_light_red", end = " ")
+    print(" ", end = " ")
+cprint(" "*5, "grey", "on_dark_grey", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "red", "on_light_red")
+
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" "*9, end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "grey", "on_dark_grey", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print()
+
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" "*9, end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "grey", "on_dark_grey", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print()
+
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" "*9, end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "grey", "on_dark_grey", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print()
+
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" "*9, end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "grey", "on_dark_grey", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print()
+
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" "*9, end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "grey", "on_dark_grey", end = " ")
+print(" ", end = " ")
+cprint(" "*5, "red", "on_light_red", end = " ")
+print()
